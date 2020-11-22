8321eeabac0d1f12f4d00f8131902f45e5722a68
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2020-11-22 11:52:58 +0100

    Create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
+import './scss/App.scss'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
 }
 
 export default App
