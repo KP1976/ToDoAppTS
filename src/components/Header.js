43fe0fb489a20ee0ec6987a683e755d9056f60a4
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2020-11-22 12:33:33 +0100

    Add Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import '../scss/Header.scss'
+import '../scss/header.scss'
+import Menu from './Menu'
 
 const Header = () => {
   return (
     <header className="header">
+      <Menu />
       <h1 className="header__title">Do zrobienia</h1>
+      <span className="header__date">20 listopada, 2020</span>
+      <div className="header__bottom-bar"></div>
     </header>
   )
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../scss/menu.scss'
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <span className="menu__bar"></span>
+    </div>
+  )
+}
+
+export default Menu
