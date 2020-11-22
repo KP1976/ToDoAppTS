461177968898f0e6a8fae3e67adb653952a24fe1
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2020-11-22 14:12:33 +0100

    Change Header componet

---
+++ src/components/Categories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="categories__container">
+        <span className="categories__number">8</span>
+        <span className="categories__text">rekreacja</span>
+      </div>
+      <div className="categories__container">
+        <span className="categories__number">11</span>
+        <span className="categories__text">tech</span>
+      </div>
+      <div className="categories__container">
+        <span className="categories__number">12</span>
+        <span className="categories__text">osobiste</span>
+      </div>
+      <div className="categories__container">
+        <span className="categories__number">5</span>
+        <span className="categories__text">jedzenie</span>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import '../scss/header.scss'
+import Categories from './Categories'
 import Menu from './Menu'
 
 const Header = () => {
   return (
     <header className="header">
-      <Menu />
-      <h1 className="header__title">Do zrobienia</h1>
-      <span className="header__date">20 listopada, 2020</span>
-      <div className="header__bottom-bar"></div>
+      <div className="header__left-side">
+        <Menu />
+        <h1 className="header__title">Do zrobienia</h1>
+        <span className="header__date">20 listopada, 2020</span>
+        <div className="header__bottom-bar"></div>
+      </div>
+      <div className="header__right-side">
+        <Categories />
+        <div className="header__tasks-done"></div>
+      </div>
     </header>
   )
 }
