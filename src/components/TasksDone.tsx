72c9db1b5b129b0acec27bcd23038d457cdaabae
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2020-12-05 15:35:56 +0100

    Add TasksDone component

---
+++ src/components/TasksDone.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TasksDone = () => {
+  return (
+    <div className="tasks-done">
+      <h3 className="task-title">skończone</h3>
+      <span className="tasks-done__number">5</span>
+    </div>
+  )
+}
+
+export default TasksDone
